a280d84155ee1292aa8545ed8387c7bebf71e0c6
nicolas_nguyen <nicosoleil69@gmail.com>
2024-01-16 12:14:19 +0100

    Add files via upload

---
+++ autoload.php
@@ -0,0 +1,6 @@
+<?php
+require_once 'models/GamesModel.php';
+require_once 'APILib/CheapShark.php';
+require_once 'Controllers/GamesController.php';
+require_once 'Controllers/DealsController.php';
+require_once 'models/Database.php';
